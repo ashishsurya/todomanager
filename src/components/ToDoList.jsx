a972c3368c60b19e5148bf115dd0a286633e459b
ashishsurya126 <ashishsurya126@gmail.com>
2021-06-22 11:53:17 +0530

    laptop change

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,30 +1,43 @@
+import { TrafficRounded } from '@material-ui/icons';
 import { useEffect, useState } from 'react';
 import { db } from '../firebase';
+import { useStateValue } from '../StateProvider';
 import './ToDoList.css';
 import ToDoListComponent from './ToDoListComponent';
 
 const ToDoList = () => {
   const [todos, setTodos] = useState([]);
 
+  const [completedTodos, setCompletedTodos] = useState([]);
+
+  const [inCompleteTodos, setInCompleteTodos] = useState([]);
+
+  // USING GLOBAL STATE
+  const [{ user }] = useStateValue();
+
   useEffect(() => {
-    const unsubscribe = () => {
-      db.collection('todos').onSnapshot((todoSnapshot) =>
+    const unsubscribe = async () => {
+      const ref = db
+        .collection('todos')
+        .where('username', '==', `${user.email}`);
+
+      await ref.onSnapshot((ss) => {
         setTodos(
-          todoSnapshot.docs.map((doc) => ({
+          ss.docs.map((doc) => ({
             id: doc.id,
             ...doc.data(),
           }))
-        )
-      );
+        );
+      });
     };
     unsubscribe();
-  }, []);
+  }, [todos, user.email]);
 
   console.log(todos);
 
   return (
     <div className='todolist'>
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <ToDoListComponent key={todo.id} {...todo} />
       ))}
     </div>
